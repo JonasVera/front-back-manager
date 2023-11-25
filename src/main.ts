8d5245f25639a46d23c9591c0adcc7ff1a9ac700
Jonas <jonas.vera@foton.la>
2023-11-25 16:39:35 -0300

    feat: habilitando api

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,17 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+import { enableProdMode } from '@angular/core';
+
+if (environment.production) {
+  enableProdMode();
+}
+
+if (location.protocol === 'https:' && environment.production) {
+  location.href = location.href.replace('https:', 'http:');
+}
+
 
 
 platformBrowserDynamic().bootstrapModule(AppModule)
